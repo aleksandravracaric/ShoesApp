9b356e3934887d227c3208e1a1562d9418742476
aleksandravracaric <aleksandravracaric997@gmail.com>
2024-03-16 18:33:41 +0100

    second commit

---
+++ src/components/ShoesHeader.js
@@ -0,0 +1,10 @@
+
+export default function ShoesHeader() {
+    return (
+        <header class="bg-primary text-center py-5 mb-4">
+            <div class="container">
+                <h1 class="fw-light text-white">Shoes store</h1>
+            </div>
+        </header>
+    )
+}
