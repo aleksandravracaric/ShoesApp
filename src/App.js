3fd00a7b4da5442be147d238f822f2f4533e7f5c
aleksandravracaric <aleksandravracaric997@gmail.com>
2024-03-24 23:34:08 +0100

    added errorHandling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import ShowDetail from './components/ShoeDetail';
+import ErrorPage from './components/ErrorPage';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Dashboard />} ></Route>
         <Route path="/shoe/" element={<ShowDetail />} ></Route>
+        <Route path="/error/" element={<ErrorPage />} ></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/services/Api.js
+++ src/services/Api.js
@@ -1,18 +1,30 @@
 import axios from "axios";
 
+
 const baseURL = 'https://shoes-collections.p.rapidapi.com';
+
+export function fetchAllShoes(){
+    
+    const headers = {
+        'X-RapidAPI-Key': '7d647b9184msh5b3e1f1fbae0e80p1591a6jsnb869ef4a2d1c',
+        'X-RapidAPI-Host': 'shoes-collections.p.rapidapi.com'
+
+    };
+    return  axios.get(`${baseURL}/shoes`, { headers: headers })
+}
 
 const fetchShoes = async () => {
 
 
-    const apiKey1 = '879a9ec3a7mshb698cb1339df89dp19bec7jsnead75be058fb'
-    const apiKey2 = '7d647b9184msh5b3e1f1fbae0e80p1591a6jsnb869ef4a2d1c'
-    const apiKey3 = '1cb2eab011msh7333949d3840809p193314jsn8e3480b7300c'
+    const apiKey1 = '879a9ec3a7mshb698cb1339df89dp19bec7jsnead75be058fb';
+    const apiKey2 = '7d647b9184msh5b3e1f1fbae0e80p1591a6jsnb869ef4a2d1c';
+    const apiKey3 = '1cb2eab011msh7333949d3840809p193314jsn8e3480b7300c';
+
     try {
 
 
         const headers = {
-            'X-RapidAPI-Key': '7d647b9184msh5b3e1f1fbae0e80p1591a6jsnb869ef4a2d1c',
+            'X-RapidAPI-Key': '879a9ec3a7mshb698cb1339df89dp19bec7jsnead75be058fb',
             'X-RapidAPI-Host': 'shoes-collections.p.rapidapi.com'
 
         };
@@ -23,6 +35,7 @@ const fetchShoes = async () => {
         return response.data;
     } catch (error) {
         console.error('Error', error);
+       
     }
 };
 export{baseURL, fetchShoes}
