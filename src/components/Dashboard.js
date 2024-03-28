b0e9815529cfd2f8659a0a70dbc8c363a46a8d6f
aleksandravracaric <aleksandravracaric997@gmail.com>
2024-03-28 22:18:39 +0100

    added loading state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,7 +6,7 @@ import ShoeGrid from "./ShoeGrid";
 import { useNavigate } from "react-router-dom";
 import { localShoes } from "../services/Mock";
 import "./Box.css";
-import Loading from "./Loading";
+
 
 
 
@@ -14,12 +14,16 @@ export default function Dashboard() {
 
     const [shoes, setShoes] = useState([]);
     const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     const navigate = useNavigate();
 
     useEffect(() => {
+
+        setLoading(true)
+
         //  Mock data
-         setShoes(localShoes) 
+        //setShoes(localShoes) 
 
         //    option 1
         // async function loadShoes() {
@@ -32,18 +36,21 @@ export default function Dashboard() {
 
 
         //      option 2
-    //     fetchAllShoes()
-    //         .then(response => {
-    //             setShoes(response.data)
-    //             setError(null)
-    //             console.log(response.data)
-    //         })
-    //         .catch(error => {
-    //             console.log(error.response.data.message)
-    //             setError(error)
-    //         })
-    // }, [])
-    })
+        fetchAllShoes()
+            .then(response => {
+                setLoading(false)
+                setShoes(response.data)
+                setError(null)
+                console.log(response.data)
+            })
+            .catch(error => {
+                console.log(error.response.data.message)
+                setLoading(false)
+                setError(error)
+            })
+
+    }, [])
+
 
     const getErrorView = () => {
         return (
@@ -61,7 +68,18 @@ export default function Dashboard() {
         )
     }
 
-
+    if (loading) {
+        return (
+            <div className="container-fluid ">
+                <div className="row">
+                    <ShoesHeader />
+                </div>
+                <div className="loadingShoes">
+                    <h1>Loading...</h1>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div className="container-fluid ">
--- src/components/Loading.js
+++
@@ -1,7 +0,0 @@
-export default function Loading(){
-    return(
-        <div className="loadingShoes">
-           Loading...Please wait...
-        </div>
-    )
-}
