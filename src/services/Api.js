c2db88375cae2d5d70cce5f26007eef3d7d2d7dd
aleksandravracaric <aleksandravracaric997@gmail.com>
2024-04-16 23:03:20 +0200

    removing some apikeys

--- src/services/Api.js
+++ src/services/Api.js
@@ -4,8 +4,6 @@ import axios from "axios";
 const baseURL = 'https://shoes-collections.p.rapidapi.com';
 
 const apiKey1 = '879a9ec3a7mshb698cb1339df89dp19bec7jsnead75be058fb';
-const apiKey2 = '7d647b9184msh5b3e1f1fbae0e80p1591a6jsnb869ef4a2d1c';
-const apiKey3 = '1cb2eab011msh7333949d3840809p193314jsn8e3480b7300c';
 const apiKey4 = 'f37705161cmshda36fceb04ba519p184d39jsncdf7b613d75c';
 
 export function fetchAllShoes() {
